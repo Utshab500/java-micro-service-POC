004702ef3c3ecfd44f86255dcbd22e9f27c26f5d
sapient.utshab-at-857496082116 <utshab.saha@publicissapient.com>
2021-01-03 01:13:22 +0530

    initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/zuul-api-gateway/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,5 @@
+
+Y
+)src/main/resources/application.properties,0\6\06f65155ec73600b9fc5c4dc2883d4bb84d8e284
+j
+:src/main/java/com/zuul/demo/ZuulApiGatewayApplication.java,c\6\c6c62cc3459373f71667c4f8089abd11a6e29ada
---
+++ zuul-api-gateway/src/main/java/com/zuul/demo/ZuulApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.zuul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@CrossOrigin
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
